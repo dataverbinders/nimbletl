7bee9894b45143f315b1a98d70d62c356fb6d70f
Eddy <eddy.tian.xing@live.nl>
2020-10-28 11:53:20 +0100

    Two new Tasks for loading into GCS and BQ
    Created two new Tasks one for loading the data in Parquet to GCS and another one to load the data from GCS into BQ. Also deleted redundant code

--- nimbletl/tasks.py
+++ nimbletl/tasks.py
@@ -14,7 +14,13 @@ import requests
 from typing import Union, Any
 from zipfile import ZipFile
 
+import pyarrow as pa
+import pyarrow.parquet as pq
+import re
+from time import sleep
+
 from google.cloud import bigquery
+from google.cloud import storage
 import pandas as pd
 import prefect
 from prefect import task
@@ -24,6 +30,7 @@ from prefect.engine.signals import SKIP
 from prefect.tasks.shell import ShellTask
 from prefect.tasks.templates import StringFormatter
 from prefect.triggers import all_successful
+from prefect.engine.results import PrefectResult
 
 from nimbletl.utilities import clean_python_name
 
@@ -132,53 +139,6 @@ def create_dir(path: Path) -> Path:
     except TypeError as error:
         print(f"Error trying to find {path}: {error!s}")
         return None
-
-
-def define_schema(url_data_definition):
-    """Define a schema of table in BigQuery, based on the data set DataProperties.
-
-    Args:
-        - url_data_definition (str): url of DataProperties data set as String.
-
-    Return:
-        - List[google.cloud.bigquery.SchemaField] 
-    """
-
-    # Get JSON format of data set.
-    url_data_info = "?".join((url_data_definition, "$format=json"))
-    # print("Data Property:", url_data_info)
-
-    data_info = requests.get(url_data_info).json() # Is of type dict()
-
-    data_info_values = data_info["value"] # Is of type list
-
-    schema_description = []
-
-    # Only dict's containing the key 'Key' has information about table columns.
-    for i in data_info_values:
-        if i["Key"] != "":
-            
-            if "Datatype" in i:
-                if i["Datatype"] == "Double":
-                    i["Datatype"] = "float"
-                
-                elif i["Datatype"] == "Long":
-                    i["Datatype"] = "integer"
-            else:
-                i["Datatype"] = "string"
-        
-            if i["Description"] is not None and len(i["Description"]) > 1024:
-                i["Description"] = i["Description"][:1021] + "..."
-            
-            schema_description.append(
-                bigquery.SchemaField(
-                    name=i["Key"],
-                    field_type=i["Datatype"],
-                    description=i["Description"]
-                ),
-            )
-
-    return schema_description
 
 
 def get_description(url_data_definition):
@@ -316,7 +276,6 @@ def cbsodatav3_to_gbq(id, third_party=False, schema="cbs", credentials=None, GCP
         item["name"]: item["url"]
     for item in requests.get(base_url[third_party]).json()["value"]
     }
-    print(urls)
 
     bq = bigquery.Client(project=GCP.project)
     job_config = bigquery.LoadJobConfig()
@@ -348,13 +307,6 @@ def cbsodatav3_to_gbq(id, third_party=False, schema="cbs", credentials=None, GCP
                     columns=lambda s: s.replace(".", "_")
                 )
 
-                """ # Define a pre-made schema for the table TypedDataSet which includes descriptions of the columns.
-                if key == "TypedDataSet":
-                    job_config.schema = define_schema(urls["DataProperties"])
-                else:
-                    # Reset job_config in order to auto-detect a schema.
-                    job_config = bigquery.LoadJobConfig() """
-
                 jobs.append(
                     bq.load_table_from_dataframe(
                         df,
@@ -372,3 +324,179 @@ def cbsodatav3_to_gbq(id, third_party=False, schema="cbs", credentials=None, GCP
                 url = None
 
     return jobs
+
+
+@task(name="get_cbs_data", result=PrefectResult())
+def cbsodatav3_to_gcs(id, third_party=False, schema="cbs", credentials=None, GCP=None, paths=None):
+    """Load CBS odata v3 into Google Cloud Storage as Parquet.
+
+    For given dataset id, following tables are uploaded into schema (taking `cbs` as default and `83583NED` as example):
+        - ``cbs.83583NED_DataProperties``: description of topics and dimensions contained in table
+        - ``cbs.83583NED_DimensionName``: separate dimension tables
+        - ``cbs.83583NED_TypedDataSet``: the TypedDataset
+        - ``cbs.83583NED_CategoryGroups``: grouping of dimensions
+
+    See `Handleiding CBS Ope Data Services (v3) <https://www.cbs.nl/-/media/statline/documenten/handleiding-cbs-opendata-services.pdf>`_ for details.
+    
+    Args:
+        - id (str): table ID like `83583NED`
+        - third_party (boolean): 'opendata.cbs.nl' is used by default (False). Set to true for dataderden.cbs.nl
+        - schema (str): schema to load data into
+        - credentials: GCP credentials
+        - GCP: config object
+        - Paths: config object for output directory
+
+    Return:
+        - Set: Paths to Parquet files
+        - String: table_description
+    """
+    
+    base_url = {
+        True: f"https://dataderden.cbs.nl/ODataFeed/odata/{id}?$format=json",
+        False: f"https://opendata.cbs.nl/ODataFeed/odata/{id}?$format=json",
+    }
+    urls = {
+        item["name"]: item["url"]
+        for item in requests.get(base_url[third_party]).json()["value"]
+    }
+
+    output = {
+        True: paths.root / paths.tmp,
+        False: paths.root / paths.cbs,
+    }
+
+    # Getting the description of the data set.
+    data_set_description = table_description(urls["TableInfos"])
+
+    gcs = storage.Client(project="dataverbinders")
+    gcs_bucket = gcs.bucket(GCP.bucket)
+
+    files_parquet = set()
+
+    # TableInfos is redundant --> use https://opendata.cbs.nl/ODataCatalog/Tables?$format=json
+    # UntypedDataSet is redundant --> use TypedDataSet
+    for key, url in [
+        (k, v) for k, v in urls.items() if k not in ("TableInfos", "UntypedDataSet")
+    ]:
+        url = "?".join((url, "$format=json"))
+        table_name = f"{schema}.{id}_{key}"
+
+        i = 0
+
+        while url:
+            logger = prefect.context.get("logger")
+            logger.info(f"Processing {key} (i = {i}) from {url}")
+            r = requests.get(url).json()
+
+            # odata api contains empty lists as values --> skip these
+            if r["value"]:
+                # DataProperties contains column odata.type --> odata_type
+                df = pd.DataFrame(r["value"]).rename(
+                    columns=lambda s: s.replace(".", "_")
+                )
+                
+                pq_dir = f"{output[third_party]}/{table_name}.parquet"
+
+                # Add path of file to set, when data set contains information
+                files_parquet.add(f"{table_name}.parquet")
+
+                cbs_table = pa.Table.from_pandas(df)
+
+                if i == 0:
+                    # Have to append the lines, instead of overwrite.
+                    # https://stackoverflow.com/questions/47113813/using-pyarrow-how-do-you-append-to-parquet-file/47114713
+                    # Use gzip as compression, is one of the accepted compression types. With default level = 6.
+                    pq_writer = pq.ParquetWriter(where=pq_dir, schema=cbs_table.schema, compression="gzip", compression_level=6)
+                    
+                # Write transformed df to the given file (pq_dir)
+                pq_writer.write_table(cbs_table)
+            
+            # each request limited to 10,000 cells
+            if "odata.nextLink" in r:
+                i += 1
+                url = r["odata.nextLink"]
+            else:
+                # Add upload to GCS here!!!
+                pq_writer.close() # Close the Parquet Writer
+
+                # Name of file in GCS.
+                gcs_blob = gcs_bucket.blob(pq_dir.split("/")[-1])
+
+                # Upload file to GCS from given location.
+                gcs_blob.upload_from_filename(filename=pq_dir)
+                
+                url = None
+
+    return files_parquet, data_set_description
+
+
+def create_dataset(name, bq_client):
+    """Creates new data set in Google BigQuery if this data set does not exist yet.
+
+    Args:
+        - name (str): Name of the data set
+        - bq_client (google.cloud.bigquery.client.Client): Google BigQuery Client
+    """
+    dataset_id = f"{bq_client.project}.{name}"
+
+    dataset_info = bigquery.Dataset(dataset_id)
+    dataset_info.description="Test for creating new Data Set"
+
+    bq_client.create_dataset(dataset_info, exists_ok=True)
+
+
+@task(name="gcs_to_bq")
+def gcs_to_bq(prefect_output, third_party=False, credentials=None, GCP=None):
+    """Load Parquet files from Google Cloud Storage into Google BigQuery.
+
+    Args:
+        - prefect_output (tuple): Output of Task "cbsodatav3_to_gcs"
+        - third_party (boolean): 'opendata.cbs.nl' is used by default (False). Set to true for dataderden.cbs.nl
+        - credentials: GCP credentials
+        - GCP: config object
+    """
+
+    awaiting_jobs = set()
+
+    def callback(future):
+        awaiting_jobs.discard(future.job_id)
+
+    logger = prefect.context.get("logger")
+    parquet_list = prefect_output[0]
+
+    bq = bigquery.Client(project=GCP.project, location=GCP.location)
+
+    # Create new Data Set if not already present.
+    data_set_id = re.split(r'[._]', list(prefect_output[0])[0])[0]
+    create_dataset(name=data_set_id, bq_client=bq)
+
+    job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.PARQUET)
+    job_config.destination_table_description=prefect_output[1]
+    jobs = []
+
+    for parquet_file in parquet_list:
+
+        gcs_url = f"gs://{GCP.bucket}/{parquet_file}"
+        table_name = parquet_file[:-8]
+        bq.delete_table(table=table_name, not_found_ok=True)
+
+        load_job = bq.load_table_from_uri(
+            gcs_url,
+            destination=table_name,
+            project=GCP.project,
+            job_config=job_config,
+        )
+
+        jobs.append(load_job)
+
+        awaiting_jobs.add(load_job.job_id)
+        load_job.add_done_callback(callback)
+    
+
+    while awaiting_jobs:
+        logger.info(f"{len(awaiting_jobs)} remaining data set(s) loading into BQ.... ")
+        sleep(1)
+    
+
+    logger.info("The data sets have been loaded into BQ")
+    
